28c3ad1c4369f9f0aaeef1d21e7ecb3279fa41ff
harisrijaa-9 <srijaa.19je0477@mech.iitism.ac.in>
2023-07-31 22:54:29 +0530

    angu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'first-app';
+  /* An empty array that is responsible
+	to add a division */
+  public items = [];
+
+  /* A two-way binding performed which
+	pushes text on division */
+  public newTask;
+
+  /* When input is empty, it will
+	not create a new division */
+  public addToList() {
+    if (this.newTask == '') {
+    } else {
+      this.items.push(this.newTask);
+      this.newTask = '';
+    }
+  }
+
+  /* This function takes to input the
+	task, that has to be deleted*/
+  public deleteTask(index) {
+    this.items.splice(index, 1);
+  }
 }
